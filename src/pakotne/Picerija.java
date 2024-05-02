1df46ad0787437b07f47f9183b2dab175dd0e8de
maksims <maksims.bizjuks@gmail.com>
2024-05-03 01:57:31 +0300

    simbolu ierobezosana

--- src/pakotne/Picerija.java
+++ src/pakotne/Picerija.java
@@ -20,10 +20,25 @@ public class Picerija {
 			switch(izvelesIndekss) {
 			case 0:
 				//pasutit picu 
-				
-				 String vards =JOptionPane.showInputDialog("Ievadi savu vārdu ");
-				String uzvards=JOptionPane.showInputDialog("Ievadi uzvārdu");
-				String telefonaNr=JOptionPane.showInputDialog("Ievadi telefona numuru");
+				String vards ;
+				do {
+				 
+				 vards=JOptionPane.showInputDialog("Ievadi savu vārdu ");
+//				 if (vards==null|| vards.isEmpty()||!Pattern.matches("[a-zA-Z]", vards)) {
+//					 JOptionPane.showMessageDialog(null, "Ievadi korrektu vārdu","Paziņojums",JOptionPane.ERROR_MESSAGE);
+//				 }
+//				 if(vards==null||vards.isEmpty()) {
+//					 JOptionPane.showMessageDialog(null, "Ievadi vārdu","Paziņojums",JOptionPane.WARNING_MESSAGE);
+//					 vards=JOptionPane.showInputDialog("Ievadi savu vārdu ");
+				}while(vards.length()<3);
+				String uzvards;
+				do {
+				 uzvards=JOptionPane.showInputDialog("Ievadi uzvārdu");
+				}while(uzvards.length()<3);
+				String telefonaNr;
+				do {
+				telefonaNr=JOptionPane.showInputDialog("Ievadi telefona numuru");
+				}while(telefonaNr.length()<7||telefonaNr.length()>9);
 				if (!Pattern.matches("\\d+", telefonaNr)) {
 					JOptionPane.showMessageDialog(null, "Telefona numur ir jārakstā ar burtiem","Kļūda ", JOptionPane.ERROR_MESSAGE);
 					telefonaNr=JOptionPane.showInputDialog("Ievadi telefona numuru");
@@ -32,7 +47,9 @@ public class Picerija {
 				String[]picasVeidi= {"Lauku","Salami","Vegetāra"};
 				int picasIzvele=JOptionPane.showOptionDialog(null, "Picas izvele", "izvēle", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, picasVeidi, picasVeidi[0]);
 				double cenas[]={5.99,6.99,7.99};
+				
 				String[] merces= {"Kečups","Majonēze","Tabasko"};
+				
 				int izveletaMerce=JOptionPane.showOptionDialog(null, "Mērču izvēle", "izvēle", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, merces, merces[0]);
 				double mercuCena[]= {0.35,0.40,0.75};
 				
