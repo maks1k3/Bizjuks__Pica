685ee275f6b75d868485cee221eb0d31a0eb33ba
maksims <maksims.bizjuks@gmail.com>
2024-05-03 01:01:09 +0300

    cenas veidošana

--- src/pakotne/Picerija.java
+++ src/pakotne/Picerija.java
@@ -1,9 +1,15 @@
 package pakotne;
 
+import java.awt.GridLayout;
 import java.util.Arrays;
 import java.util.regex.Pattern;
 
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 public class Picerija {
 private static CilvekaDati cilvekaDati=null;
@@ -27,7 +33,34 @@ private static CilvekaDati cilvekaDati=null;
 					telefonaNr=JOptionPane.showInputDialog("Ievadi telefona numuru");
 				}
 				cilvekaDati= new CilvekaDati(vards,uzvards,telefonaNr);
-				break;
+				
+				
+				JFrame picaLogs=new JFrame("Izvēlies");
+				picaLogs.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				picaLogs.setSize(700,700);
+				
+				JPanel picas=new JPanel();
+				picas.setLayout(new GridLayout(3,1));
+				
+				JRadioButton salami=new JRadioButton(new ImageIcon("pakotne.salami.png"));
+				JRadioButton lauku=new JRadioButton(new ImageIcon("lauku.png"));
+				JRadioButton vegetara=new JRadioButton(new ImageIcon("vegetara.png"));
+				
+				ButtonGroup grupa=new ButtonGroup();
+				grupa.add(salami);
+				grupa.add(lauku);
+				grupa.add(vegetara);
+				
+				
+				picas.add(salami);
+				picas.add(lauku);
+				picas.add(vegetara);
+				
+				
+				picaLogs.add(picas);
+				
+				int picasIzvele=JOptionPane.showOptionDialog(picaLogs, picas, "Izvēlies picu", JOptionPane.DEFAULT_OPTION,JOptionPane.PLAIN_MESSAGE,null,new Object[] {salami,lauku,vegetara},salami);
+				picaLogs.setVisible(true);
 			case 1:
 				break;
 			case 2:
