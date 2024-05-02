4c78cb3458f4323121dfcf07f3de87f75ed77800
maksims <maksims.bizjuks@gmail.com>
2024-05-02 23:01:25 +0300

    taisu jauno klasi par cilveka info

--- src/pakotne/Picerija.java
+++ src/pakotne/Picerija.java
@@ -1,9 +1,23 @@
 package pakotne;
 
+import javax.swing.JOptionPane;
+
 public class Picerija {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String izvele;
+		int izvelesIndekss = 0;
+		String []darbibas={"Pasūtīt picu","Apskatīt pasūtījumus","Sūtītāja info","Čeks","Apturēt"};
+		do {
+			izvele=(String)JOptionPane.showInputDialog(null,"Izvēlies darbību","Izvēle",JOptionPane.QUESTION_MESSAGE,null, darbibas, darbibas[0]);
+			
+			switch(izvelesIndekss) {
+			case 0:
+				//pasutit picu 
+				String vards =JOptionPane.showInputDialog("Ievadi savu vārdu ");
+				
+			}
+		}while(izvelesIndekss!=5);
 
 	}
 
