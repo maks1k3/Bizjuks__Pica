395efc918df2c392a82742764aa13c8085ea94c4
maksims <maksims.bizjuks@gmail.com>
2024-05-02 23:32:23 +0300

    Uzstaisit case cilveka informacija

--- src/pakotne/CilvekaDati.java
+++ src/pakotne/CilvekaDati.java
@@ -1,5 +1,18 @@
 package pakotne;
 
-public class CilvekaDati {
+import javax.swing.JOptionPane;
 
+public class CilvekaDati {
+private String vards;
+private String uzvards;
+private String telefonaNr;
+
+public CilvekaDati(String vards,String uzvards, String telefonaNr) {
+	this.vards=vards;
+	this.uzvards=uzvards;
+	this.telefonaNr=telefonaNr;
+}
+public void  infoIzvade(){
+	JOptionPane.showMessageDialog(null, "Vārds: "+vards+ "\nUzvārds: "+uzvards+"\nTelefona Numurs: "+telefonaNr,"Cilvēka dati",JOptionPane.INFORMATION_MESSAGE);
+}
 }
--- src/pakotne/Picerija.java
+++ src/pakotne/Picerija.java
@@ -1,21 +1,42 @@
 package pakotne;
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 import javax.swing.JOptionPane;
 
 public class Picerija {
-
+private static CilvekaDati cilvekaDati=null;
 	public static void main(String[] args) {
 		String izvele;
 		int izvelesIndekss = 0;
 		String []darbibas={"Pasūtīt picu","Apskatīt pasūtījumus","Sūtītāja info","Čeks","Apturēt"};
 		do {
-			izvele=(String)JOptionPane.showInputDialog(null,"Izvēlies darbību","Izvēle",JOptionPane.QUESTION_MESSAGE,null, darbibas, darbibas[0]);
 			
+			izvele=(String)JOptionPane.showInputDialog(null,"Izvēlies darbību","Izvēle",JOptionPane.QUESTION_MESSAGE,null, darbibas, darbibas[0]);
+			izvelesIndekss=Arrays.asList(darbibas).indexOf(izvele);
 			switch(izvelesIndekss) {
 			case 0:
 				//pasutit picu 
-				String vards =JOptionPane.showInputDialog("Ievadi savu vārdu ");
 				
+				 String vards =JOptionPane.showInputDialog("Ievadi savu vārdu ");
+				String uzvards=JOptionPane.showInputDialog("Ievadi uzvārdu");
+				String telefonaNr=JOptionPane.showInputDialog("Ievadi telefona numuru");
+				if (!Pattern.matches("\\d+", telefonaNr)) {
+					JOptionPane.showMessageDialog(null, "Telefona numur ir jārakstā ar burtiem","Kļūda ", JOptionPane.ERROR_MESSAGE);
+					telefonaNr=JOptionPane.showInputDialog("Ievadi telefona numuru");
+				}
+				cilvekaDati= new CilvekaDati(vards,uzvards,telefonaNr);
+				break;
+			case 1:
+				break;
+			case 2:
+				if(cilvekaDati!=null) {
+			     cilvekaDati.infoIzvade();
+				}else {
+					JOptionPane.showMessageDialog(null, "Vēl nav informācijas ","Paziņojums",JOptionPane.PLAIN_MESSAGE);
+				}
+				break;
 			}
 		}while(izvelesIndekss!=5);
 
